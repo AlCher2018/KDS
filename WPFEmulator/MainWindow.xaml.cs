8ac04db3c1082b0fa6c194cacb47456d222c8472
Integra-ProgC# <ProgC# @ wherenever.com>
2017-03-26 17:18:20 +0300

    2017-03-25 commit01

---
+++ WPFEmulator/MainWindow.xaml.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFEmulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<genOrder> _orders = new ObservableCollection<genOrder>();
+        private ObservableCollection<genOrderStatus> _ordersStatus = new ObservableCollection<genOrderStatus>();
+        private Timer _orderTimer = new Timer();
+        private Random rnd = new Random();
+
+        private int _currNumber = 123;
+        private object _threadLockObj;
+        private KDSContext _db;
+        private bool _enableEvents;
+
+        private string[] _statusNames = { "Готовится", "ГОТОВ", "ВЫДАН" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _threadLockObj = new object();
+            _db = new KDSContext();
+            _orderTimer.AutoReset = false;
+            _orderTimer.Elapsed += _orderTimer_Elapsed;
+
+            lbOrders.ItemsSource = _ordersStatus;
+            //rbAuto.IsChecked = true;
+            rbManual.IsChecked = true;
+        }
+
+        private void rbAuto_Checked(object sender, RoutedEventArgs e)
+        {
+            createAutoOrder();
+        }
+
+        #region auto create order
+        private void createAutoOrder()
+        {
+            _orderTimer.Interval = rnd.Next(3, 5) * 1000d;
+            _orderTimer.Start();
+        }
+
+        private void _orderTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Dispatcher.Invoke((Action)delegate
+            {
+                genOrder order = new genOrder(rbAuto.IsChecked ?? false) { Number = _currNumber++, Date = DateTime.Now };
+                order.OrderStatusChanged += NewOrder_StatusEventHandler;
+                lock (_threadLockObj)
+                {
+                    _db.Order.Add(new Order() { CreateDate = order.Date, LanguageTypeId = 2, Number = order.Number, QueueStatusId = order.StatusId });
+                    _db.SaveChanges();
+
+                    _orders.Add(order);
+                    addListStatusItem(order, true);
+                }
+
+                createAutoOrder();
+            });
+        }
+
+        private void NewOrder_StatusEventHandler(object sender, genOrderStatusChangedArgs e)
+        {
+            this.Dispatcher.Invoke(() => 
+            {
+                genOrder order = (genOrder)sender;
+
+                if (e.StatusId == 1)
+                {
+                    lock (_threadLockObj)
+                    {
+                        addListStatusItem(order, false);
+                        setDBOrderStatus(order.Number, order.StatusId);
+                    }
+                }
+                else if (e.StatusId == 2)
+                {
+                    lock (_threadLockObj)
+                    {
+                        addListStatusItem(order, false);
+                        setDBOrderStatus(order.Number, order.StatusId);
+
+                        order.OrderStatusChanged -= NewOrder_StatusEventHandler;
+                        _orders.Remove(order);
+                        order = null;
+                    }
+                }
+            });
+        }
+
+        private void addListStatusItem(genOrder order, bool isNew)
+        {
+            genOrderStatus os = new genOrderStatus() { Number = order.Number, Date = isNew ? order.Date : DateTime.Now, StatusName = order.StatusName };
+            _ordersStatus.Add(os);
+            lbOrders.ScrollIntoView(os);
+        }
+
+        private void setDBOrderStatus(int number, int status)
+        {
+            Order dbOrder = _db.Order.FirstOrDefault(o => o.Number == number);
+            if (dbOrder != null)
+            {
+                dbOrder.QueueStatusId = status;
+                _db.SaveChanges();
+            }
+        }
+        #endregion
+
+        private void rbManual_Checked(object sender, RoutedEventArgs e)
+        {
+            _orderTimer.Stop();
+        }
+
+        private FrameworkElement FindVisualChildrenByName(DependencyObject objectFrom, string childName)
+        {
+            if (objectFrom == null) return null;
+
+            FrameworkElement retVal = null;
+            int iCount = VisualTreeHelper.GetChildrenCount(objectFrom);
+            for(int i = 0; i < iCount; i++)
+            {
+                DependencyObject rawChild = VisualTreeHelper.GetChild(objectFrom, i);
+                if (rawChild is FrameworkElement)
+                {
+                    FrameworkElement curObj = (rawChild as FrameworkElement);
+                    if (curObj.Name == childName) retVal = curObj;
+                    else retVal = FindVisualChildrenByName(curObj, childName);
+                }
+                if (retVal != null) return retVal;
+            }
+
+            return null;
+        }
+
+        private void createOtrder_Click(object sender, RoutedEventArgs e)
+        {
+            genOrder newOrder = new genOrder(false) { Number = _currNumber++, Date = DateTime.Now };
+            lock (_threadLockObj)
+            {
+                _db.Order.Add(new Order() { CreateDate = newOrder.Date, LanguageTypeId = 2, Number = newOrder.Number, QueueStatusId = 0 });
+                _db.SaveChanges();
+
+                _orders.Add(newOrder);
+                addListStatusItem(newOrder, true);
+            }
+        }
+
+        private void lbOrders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (rbManual.IsChecked == false) return;
+
+            genOrderStatus os = (genOrderStatus)(sender as ListBox).SelectedItem;
+            tbOrderNumber.Text = os.Number.ToString();
+
+            setCheckBoxesValue(os);
+        }
+
+        private void cbReady_Checked(object sender, RoutedEventArgs e)
+        {
+            if (_enableEvents == false) return;
+
+            genOrderStatus os = (genOrderStatus)lbOrders.SelectedItem;
+            if (os.StatusName == _statusNames[2]) return;
+
+            os.StatusName = _statusNames[1];
+            updateBindStatus(os);
+        }
+
+        private void cbReady_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (_enableEvents == false) return;
+
+            genOrderStatus os = (genOrderStatus)lbOrders.SelectedItem;
+            if (os.StatusName == _statusNames[2]) return;
+
+            os.StatusName = _statusNames[0];
+            updateBindStatus(os);
+        }
+
+        private void cbOut_Checked(object sender, RoutedEventArgs e)
+        {
+            if (_enableEvents == false) return;
+
+            genOrderStatus os = (genOrderStatus)lbOrders.SelectedItem;
+
+            os.StatusName = _statusNames[2];
+            updateBindStatus(os);
+        }
+
+        private void cbOut_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (_enableEvents == false) return;
+
+            genOrderStatus os = (genOrderStatus)lbOrders.SelectedItem;
+
+            os.StatusName = _statusNames[1];
+            updateBindStatus(os);
+        }
+
+        private void updateBindStatus(genOrderStatus os)
+        {
+            DependencyObject obj = lbOrders.ItemContainerGenerator.ContainerFromItem(os);
+            FrameworkElement tbStatus = FindVisualChildrenByName((FrameworkElement)obj, "tbStatus");
+            if (tbStatus != null)
+            {
+                BindingExpression be = (tbStatus as TextBlock).GetBindingExpression(TextBlock.TextProperty);
+                if (be != null) be.UpdateTarget();
+            }
+
+            setCheckBoxesValue(os);
+        }
+        private void setCheckBoxesValue(genOrderStatus os)
+        {
+            bool bVal;
+
+            if (os.StatusName == _statusNames[0])
+            {
+                _enableEvents = false;
+                cbReady.IsChecked = false;
+                cbOut.IsChecked = false;
+                _enableEvents = true;
+                return;
+            }
+
+            _enableEvents = false;
+
+            bVal = (os.StatusName == _statusNames[1]);
+            if ((cbReady.IsChecked ?? false) != bVal) cbReady.IsChecked = bVal;
+
+            bVal = (os.StatusName == _statusNames[2]);
+            if ((cbOut.IsChecked ?? false) != bVal) cbOut.IsChecked = bVal;
+
+            _enableEvents = true;
+        }
+
+
+
+    }  // class
+}
---
+++ WPFEmulator/genOrder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WPFEmulator
+{
+    public class genOrder
+    {
+        private string[] _statusNames = { "Готовится", "ГОТОВ", "ВЫДАН" };
+
+        private Timer _timer;
+        private Random _rnd;
+
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+
+        public event EventHandler<genOrderStatusChangedArgs> OrderStatusChanged;
+
+        private int _statusId;
+        public int StatusId { get { return _statusId; } }
+        public string StatusName { get { return _statusNames[StatusId]; } }
+
+        public genOrder(bool isAutoChangeStatus)
+        {
+            _rnd = new Random();
+            _timer = new Timer();
+            _timer.AutoReset = false;
+            _timer.Elapsed += _timer_Elapsed;
+
+            _statusId = 0;
+
+            if (isAutoChangeStatus)
+            {
+                _timer.Interval = _rnd.Next(3, 7) * 1000d;
+                _timer.Start();
+            }
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _statusId++;
+
+            if (OrderStatusChanged != null) OrderStatusChanged(this, new genOrderStatusChangedArgs() { StatusId = _statusId, StatusName = this.StatusName });
+
+            if (_statusId == 2)
+            {
+                _timer.Stop(); _timer.Close(); _timer = null;
+            }
+            else
+            {
+                _timer.Interval = _rnd.Next(3, 7) * 1000d;
+                _timer.Start();
+            }
+        }
+    }  // class genOrder
+
+    public class genOrderStatusChangedArgs: EventArgs
+    {
+        public int StatusId { get; set; }
+        public string StatusName { get; set; }
+    }
+
+}
