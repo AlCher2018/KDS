15c6948a0fcc94e7ba2813b47e38dca231bd2fb3
Integra-Prog01 <ProgC# @ wherenever.com>
2017-05-22 22:50:19 +0300

    2017-05-22 commit02

--- KDSWPFClient/View/StateChange.xaml.cs
+++ KDSWPFClient/View/StateChange.xaml.cs
@@ -37,12 +37,11 @@ namespace KDSWPFClient.View
             InitializeComponent();
 
             this.Loaded += StateChange_Loaded;
-
-            IsShowTitle = true;
         }
 
         private void StateChange_Loaded(object sender, RoutedEventArgs e)
         {
+            IsShowTitle = true;
             setModelType();
 
             _allowedStates = (List<KeyValuePair<OrderStatusEnum, OrderStatusEnum>>)AppLib.GetAppGlobalValue("StatesAllowedForMove");
@@ -110,11 +109,12 @@ namespace KDSWPFClient.View
             // если есть объект приложения (заказ / блюдо), то настроить кнопки переходов в другие состояния
             if ((_modelType == AppViewModelEnum.Order) || (_modelType == AppViewModelEnum.Dish))
             {
+                // текущее состояние
                 OrderStatusEnum currentState = (OrderStatusEnum)((_modelType == AppViewModelEnum.Order) ? Order.OrderStatusId : (Dish.DishStatusId ?? 0));
                 // из разрешенных переходов выбрать переходы для текущего состояния
-                List<KeyValuePair<OrderStatusEnum, OrderStatusEnum>> allowedStatesForCurrent  = new List<KeyValuePair<OrderStatusEnum, OrderStatusEnum>>(_allowedStates.Where(states => states.Key == currentState));
+                List<KeyValuePair<OrderStatusEnum, OrderStatusEnum>> allowedStatesForCurrentState  = new List<KeyValuePair<OrderStatusEnum, OrderStatusEnum>>(_allowedStates.Where(states => states.Key == currentState));
 
-                if ((allowedStatesForCurrent == null) || (allowedStatesForCurrent.Count == 0))
+                if ((allowedStatesForCurrentState == null) || (allowedStatesForCurrentState.Count == 0))
                 {
                     tbNoAllowedStates.Visibility = Visibility.Visible;
                     pnlStateButtons.Visibility = Visibility.Hidden;
@@ -127,19 +127,37 @@ namespace KDSWPFClient.View
                 // есть переходы для текущего состояния - настроить размеры кнопок переходов
                 if (pnlStateButtons.Visibility == Visibility.Visible)
                 {
-                    int statesCount = allowedStatesForCurrent.Count;
+                    int statesCount = allowedStatesForCurrentState.Count;
+                    // количество кнопок смены состояния
+                    // 1 - одна, большая, 2 - две на стандартную ширину, 3 и более - расширение окна по горизонтали, добавление в StackPanel
                     if (statesCount == 1)
                     {
-                        btnState2.Visibility = Visibility.Collapsed;
-                        btnState1.Width = 0.7 * pnlWidth; btnState1.Height = 0.7
+                        Border btnState = getStateButton(0.7 * pnlWidth, 0.7 * pnlHeight, allowedStatesForCurrentState[0].Value);
+                        double horMargin = Math.Floor(0.15 * pnlWidth);
+                        btnState.Margin = new Thickness(horMargin, 0.15d * pnlHeight, horMargin, 0.15d * pnlHeight);
+                        pnlStateButtons.Children.Add(btnState);
                     }
                     else if (statesCount == 2)
                     {
-
+                        Border btnState = null;
+                        foreach (KeyValuePair<OrderStatusEnum, OrderStatusEnum> item in allowedStatesForCurrentState)
+                        {
+                            btnState = getStateButton(0.35 * pnlWidth, 0.7 * pnlHeight, item.Value);
+                            btnState.Margin = new Thickness(0.1d * pnlWidth, 0.15d * pnlHeight, 0.1d * pnlWidth, 0.15d * pnlHeight);
+                            pnlStateButtons.Children.Add(btnState);
+                        }
                     }
                     else
                     {
-
+                        Border btnState = null;
+                        double wBtn = 0.35d * pnlWidth, mBtn = 0.1d * pnlWidth;
+                        this.Width = (wBtn + mBtn) * allowedStatesForCurrentState.Count + 0.1d * pnlWidth;
+                        foreach (KeyValuePair<OrderStatusEnum, OrderStatusEnum> item in allowedStatesForCurrentState)
+                        {
+                            btnState = getStateButton(0.35 * pnlWidth, 0.7 * pnlHeight, item.Value);
+                            btnState.Margin = new Thickness(0.1d * pnlWidth, 0.15d * pnlHeight, 0.1d * pnlWidth, 0.15d * pnlHeight);
+                            pnlStateButtons.Children.Add(btnState);
+                        }
                     }
                 }
             }
@@ -153,6 +171,115 @@ namespace KDSWPFClient.View
 
         }
 
+        private Border getStateButton(double width, double height, OrderStatusEnum eState)
+        {
+            double cornerRadius = 0.1 * height;
+
+            Border retBorder = new Border()
+            {
+                Width = width, Height = height, BorderThickness = new Thickness(2d), BorderBrush = Brushes.DarkGray,
+                CornerRadius = new CornerRadius(cornerRadius)
+            };
+
+            Grid grd = new Grid();
+            double hRow = 0.4 * height;
+            grd.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(hRow, GridUnitType.Pixel) });
+            grd.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(0.2*height, GridUnitType.Pixel) });
+            grd.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(hRow, GridUnitType.Pixel) });
+
+            TextBlock tbStateName = new TextBlock()
+            {
+                VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center,
+                Text = getStateButtonText(eState).ToUpper(), FontWeight = FontWeights.Bold, FontSize = 0.2d * height
+            };
+            tbStateName.SetValue(Grid.RowProperty, 0);
+            grd.Children.Add(tbStateName);
+
+            Viewbox vbDescr = new Viewbox()
+            {
+                Stretch = Stretch.Uniform, StretchDirection = StretchDirection.DownOnly, Width = width, Height = hRow,
+                Margin = new Thickness(cornerRadius),
+                Child = new TextBlock()
+                {
+                    TextWrapping = TextWrapping.Wrap, VerticalAlignment = VerticalAlignment.Center,
+                    Text = getStateDescr(eState)
+                }
+            };
+            vbDescr.SetValue(Grid.RowProperty, 2);
+            grd.Children.Add(vbDescr);
+
+
+            retBorder.Child = grd;
+            return retBorder;
+        }
+
+        private string getStateDescr(OrderStatusEnum eState)
+        {
+            string retVal = null;
+            switch (eState)
+            {
+                case OrderStatusEnum.None:
+                    break;
+                case OrderStatusEnum.WaitingCook:
+                    break;
+                case OrderStatusEnum.Cooking:
+                    retVal = "Начать приготовление блюда/заказа";
+                    break;
+                case OrderStatusEnum.Ready:
+                    retVal = "Закончить приготовление блюда/заказа";
+                    break;
+                case OrderStatusEnum.Took:
+                    retVal = "Забрать блюдо/заказ и отнести его Клиенту";
+                    break;
+                case OrderStatusEnum.Cancelled:
+                    retVal = "Отменить приготовление блюда/заказа";
+                    break;
+                case OrderStatusEnum.Commit:
+                    retVal = "Зафиксировать, т.е. запретить изменять статус блюда/заказа";
+                    break;
+                case OrderStatusEnum.CancelConfirmed:
+                    retVal = "Подтвердить отмену блюда/заказа";
+                    break;
+                default:
+                    break;
+            }
+
+            return retVal;
+        }
+
+        private string getStateButtonText(OrderStatusEnum eState)
+        {
+            string retVal = null;
+            switch (eState)
+            {
+                case OrderStatusEnum.None:
+                    break;
+                case OrderStatusEnum.WaitingCook:
+                    retVal = "Ожидание";
+                    break;
+                case OrderStatusEnum.Cooking:
+                    retVal = "Готовить";
+                    break;
+                case OrderStatusEnum.Ready:
+                    retVal = "Закончить";
+                    break;
+                case OrderStatusEnum.Took:
+                    retVal = "Забрать";
+                    break;
+                case OrderStatusEnum.Cancelled:
+                    retVal = "Отменить";
+                    break;
+                case OrderStatusEnum.Commit:
+                    retVal = "Зафиксировать";
+                    break;
+                case OrderStatusEnum.CancelConfirmed:
+                    retVal = "Подтвердить отмену";
+                    break;
+                default:
+                    break;
+            }
+            return retVal;
+        }
 
         private void setModelType()
         {
